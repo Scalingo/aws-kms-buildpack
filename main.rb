e2e56f16550c74bdba53b7c3bb4b9e9e32100921
Jonathan Hurter <john@scalingo.com>
2017-03-16 21:06:24 +0100

    First commit

---
+++ main.rb
@@ -0,0 +1,34 @@
+require 'aws-sdk'
+
+def download(s3, install_path, key)
+  resp = s3.get_object({
+    bucket: ENV["AWS_BUCKET"],
+    key: key,
+    ssekms_key_id: ENV["KMS_KEY_ID"],
+    response_target: install_path,
+  })
+end
+
+needed = ["AWS_BUCKET", "AWS_REGION", "AWS_ID", "AWS_TOKEN", "CERTS_INSTALL_PATH", "CA_KEY", "CERT_KEY", "PRIVATE_KEY"]
+
+needed.each do |key|
+  if ! ENV.has_key?(key) then
+    abort("Missing key: #{key}")
+  end
+end
+
+Aws.config.update({
+  region: ENV["AWS_REGION"],
+  credentials: Aws::Credentials.new(ENV["AWS_ID"], ENV["AWS_TOKEN"])
+})
+
+
+s3 = Aws::S3::Client.new({region: ENV["AWS_REGION"]})
+
+download(s3, "#{ENV["CERTS_INSTALL_PATH"]}/ca.crt", ENV["CA_KEY"])
+download(s3, "#{ENV["CERTS_INSTALL_PATH"]}/cert.crt", ENV["CERT_KEY"])
+download(s3, "#{ENV["CERTS_INSTALL_PATH"]}/private.key", ENV["PRIVATE_KEY"])
+
+
+
+
