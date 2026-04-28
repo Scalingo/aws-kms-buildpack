33d2663e8691fd67b78ad97601a2533d4565a981
Samuel Maftoul <samuel.maftoul@scalingo.com>
2026-04-28 14:53:35 +0200

    feat: build a download plan to avoid downloading twice the zipfile to really avoid race condition

--- support/kmsbp/kmsbp.go
+++ support/kmsbp/kmsbp.go
@@ -23,6 +23,17 @@ var (
 
 const zipSpecSeparator = ":"
 
+type downloadItem struct {
+	dest  string
+	obj   string
+	entry string
+}
+
+type downloadPlan struct {
+	plain map[string]string         // object -> dest
+	zips  map[string][]downloadItem // zipObject -> []items
+}
+
 func main() {
 	validateEnv()
 	objects := strings.Split(os.Getenv("OBJECTS"), ",")
@@ -43,28 +54,53 @@ func main() {
 		os.Exit(-1)
 	}
 
+	// Parse and build execution plan
+	plan, err := buildDownloadPlan(objects, files, basePath)
+	if err != nil {
+		log.Println(err)
+		os.Exit(-1)
+	}
+
+	// Execute plan
 	downloader := s3manager.NewDownloader(awsConfig())
-	for i, object := range objects {
-		object = strings.TrimSpace(object)
-		file := strings.TrimSpace(files[i])
 
-		objectKey, zipEntry, isZipEntry, err := parseObjectSpec(object)
+	if err := executePlan(downloader, plan); err != nil {
+		log.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func buildDownloadPlan(objectSpecs, fileDestinations []string, basePath string) (*downloadPlan, error) {
+	plan := &downloadPlan{
+		plain: make(map[string]string),
+		zips:  make(map[string][]downloadItem),
+	}
+
+	for i, spec := range objectSpecs {
+		spec = strings.TrimSpace(spec)
+		dest := strings.TrimSpace(fileDestinations[i])
+		fullDest := filepath.Join(basePath, dest)
+
+		objectKey, zipEntry, isZipEntry, err := parseObjectSpec(spec)
 		if err != nil {
-			log.Println(err)
-			os.Exit(-1)
+			return nil, err
 		}
 
-		dest := filepath.Join(basePath, file)
 		if isZipEntry {
-			err = downloadFromZipObject(downloader, dest, objectKey, zipEntry)
+			plan.zips[objectKey] = append(plan.zips[objectKey], downloadItem{
+				dest:  fullDest,
+				obj:   objectKey,
+				entry: zipEntry,
+			})
 		} else {
-			err = download(downloader, dest, objectKey)
-		}
-		if err != nil {
-			log.Println("fail to download object", object)
-			os.Exit(-1)
+			if existing, ok := plan.plain[objectKey]; ok && existing != fullDest {
+				return nil, fmt.Errorf("object %q mapped to different destinations: %q and %q", objectKey, existing, fullDest)
+			}
+			plan.plain[objectKey] = fullDest
 		}
 	}
+
+	return plan, nil
 }
 
 func parseObjectSpec(spec string) (string, string, bool, error) {
@@ -82,15 +118,39 @@ func parseObjectSpec(spec string) (string, string, bool, error) {
 	return object, entry, true, nil
 }
 
-func download(downloader *s3manager.Downloader, dest string, object string) error {
+func executePlan(downloader *s3manager.Downloader, plan *downloadPlan) error {
+	// Download plain objects
+	for objectKey, dest := range plan.plain {
+		if err := downloadRawObject(downloader, dest, objectKey); err != nil {
+			return fmt.Errorf("failed to download object %q: %w", objectKey, err)
+		}
+	}
+
+	// Download and extract zip objects
+	for zipObject, items := range plan.zips {
+		tmpPath, err := downloadZipTempFile(downloader, zipObject)
+		if err != nil {
+			return err
+		}
+		defer os.Remove(tmpPath)
+
+		if err := extractZipEntries(tmpPath, items); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func downloadRawObject(downloader *s3manager.Downloader, dest, object string) error {
 	log.Println("Downloading", object, "to", dest)
 	if err := os.MkdirAll(filepath.Dir(dest), 0700); err != nil {
-		return fmt.Errorf("failed to create destination directory for %q, %v", dest, err)
+		return fmt.Errorf("failed to create destination directory for %q: %w", dest, err)
 	}
 
 	f, err := os.Create(dest)
 	if err != nil {
-		return fmt.Errorf("failed to create file %q, %v", dest, err)
+		return fmt.Errorf("failed to create file %q: %w", dest, err)
 	}
 	defer f.Close()
 
@@ -99,74 +159,92 @@ func download(downloader *s3manager.Downloader, dest string, object string) erro
 		Key:    aws.String(object),
 	})
 	if err != nil {
-		return fmt.Errorf("failed to upload file, %v", err)
+		return fmt.Errorf("failed to download file: %w", err)
 	}
 	return nil
 }
 
-func downloadFromZipObject(downloader *s3manager.Downloader, dest string, object string, zipEntry string) error {
-	log.Println("Downloading", zipEntry, "from", object, "to", dest)
-
+func downloadZipTempFile(downloader *s3manager.Downloader, zipObject string) (string, error) {
 	tmp, err := ioutil.TempFile("", "kmsbp-*.zip")
 	if err != nil {
-		return fmt.Errorf("failed to create temporary file for zip object %q, %v", object, err)
+		return "", fmt.Errorf("failed to create temporary file for zip object %q: %w", zipObject, err)
 	}
 	tmpPath := tmp.Name()
-	defer os.Remove(tmpPath)
 
 	_, err = downloader.Download(tmp, &s3.GetObjectInput{
 		Bucket: aws.String(os.Getenv("KMSBP_AWS_BUCKET")),
-		Key:    aws.String(object),
+		Key:    aws.String(zipObject),
 	})
 	if closeErr := tmp.Close(); closeErr != nil && err == nil {
 		err = closeErr
 	}
 	if err != nil {
-		return fmt.Errorf("failed to download zip object %q, %v", object, err)
+		_ = os.Remove(tmpPath)
+		return "", fmt.Errorf("failed to download zip object %q: %w", zipObject, err)
 	}
 
-	zr, err := zip.OpenReader(tmpPath)
+	return tmpPath, nil
+}
+
+func extractZipEntries(zipPath string, items []downloadItem) error {
+	zr, err := zip.OpenReader(zipPath)
 	if err != nil {
-		return fmt.Errorf("failed to open zip object %q, %v", object, err)
+		return fmt.Errorf("failed to open zip file %q: %w", zipPath, err)
 	}
 	defer zr.Close()
 
-	for _, file := range zr.File {
-		if file.Name != zipEntry {
-			continue
-		}
-		if file.FileInfo().IsDir() {
-			return fmt.Errorf("zip entry %q from object %q is a directory", zipEntry, object)
+	for _, item := range items {
+		found := false
+		for _, file := range zr.File {
+			if file.Name != item.entry {
+				continue
+			}
+			found = true
+
+			if file.FileInfo().IsDir() {
+				return fmt.Errorf("zip entry %q is a directory", item.entry)
+			}
+
+			in, err := file.Open()
+			if err != nil {
+				return fmt.Errorf("failed to open zip entry %q: %w", item.entry, err)
+			}
+
+			if err := os.MkdirAll(filepath.Dir(item.dest), 0700); err != nil {
+				_ = in.Close()
+				return fmt.Errorf("failed to create destination directory for %q: %w", item.dest, err)
+			}
+
+			out, err := os.Create(item.dest)
+			if err != nil {
+				_ = in.Close()
+				return fmt.Errorf("failed to create file %q: %w", item.dest, err)
+			}
+
+			_, copyErr := io.Copy(out, in)
+			inCloseErr := in.Close()
+			outCloseErr := out.Close()
+
+			if copyErr != nil {
+				return fmt.Errorf("failed to write zip entry %q to %q: %w", item.entry, item.dest, copyErr)
+			}
+			if inCloseErr != nil {
+				return fmt.Errorf("failed to close zip entry %q: %w", item.entry, inCloseErr)
+			}
+			if outCloseErr != nil {
+				return fmt.Errorf("failed to close file %q: %w", item.dest, outCloseErr)
+			}
+
+			log.Println("Downloaded", item.entry, "from", item.obj, "to", item.dest)
+			break
 		}
 
-		in, err := file.Open()
-		if err != nil {
-			return fmt.Errorf("failed to open zip entry %q from object %q, %v", zipEntry, object, err)
+		if !found {
+			return fmt.Errorf("zip entry %q not found in %q", item.entry, zipPath)
 		}
-		defer in.Close()
-
-		if err := os.MkdirAll(filepath.Dir(dest), 0700); err != nil {
-			return fmt.Errorf("failed to create destination directory for %q, %v", dest, err)
-		}
-
-		out, err := os.Create(dest)
-		if err != nil {
-			return fmt.Errorf("failed to create file %q, %v", dest, err)
-		}
-
-		_, copyErr := io.Copy(out, in)
-		closeErr := out.Close()
-		if copyErr != nil {
-			return fmt.Errorf("failed to write zip entry %q to %q, %v", zipEntry, dest, copyErr)
-		}
-		if closeErr != nil {
-			return fmt.Errorf("failed to close file %q, %v", dest, closeErr)
-		}
-
-		return nil
 	}
 
-	return fmt.Errorf("zip entry %q not found in object %q", zipEntry, object)
+	return nil
 }
 
 func awsConfig() aws.Config {
